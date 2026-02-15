2daecd63c136b0b18d16c6bed8a8f026712cbe6b
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2026-02-15 23:39:35 +0530

    used methods of studentDAO class

--- src/main/java/com/harshil/student/service/StudentService.java
+++ src/main/java/com/harshil/student/service/StudentService.java
@@ -23,14 +23,7 @@ public class StudentService{
         while(true){
             System.out.print("Enter email: ");
             email = sc.nextLine();
-            for(Student s : studentDetails){
-                if(email.equals(s.getEmail())){
-                    System.out.println("Email already exists.");
-                    email = null;
-                    break;
-                }
-            }
-            if(null != email) break;
+            if(null == sDao.searchByEmail(email)) break;
         }
         int age;
         while(true){
@@ -56,13 +49,9 @@ public class StudentService{
         System.out.print("Enter your course: ");
         String course = sc.nextLine();
         Student s = new Student(name , email , age , course);
-        try{
-            sDao.save(s);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+        int id = sDao.save(s);
         System.out.println("Student added successfully!");
-        System.out.println("Student ID: " + s.getId());
+        System.out.println("Student ID: " + id);
     }
 
     public void view(){
@@ -71,46 +60,37 @@ public class StudentService{
         System.out.println("ID Name Email Age Course");
         System.out.println(lines);
 
-        ArrayList<Student> allStudents = null;
-        try{
-            allStudents = sDao.view();
-        }catch(Exception e){
-            // e.printStackTrace();
-            System.out.println("Error");
-        }
-
+        ArrayList<Student> allStudents = sDao.view();
         for(Student s : allStudents){
             System.out.println(s.getId() + s.getName() + s.getEmail() + s.getAge() + s.getCourse());
         }
         System.out.println(lines);
-        System.out.println("Total Students: " + studentDetails.size());
+        System.out.println("Total Students: " + allStudents.size());
     }
 
     public void update(Scanner sc){
-        System.out.print("Enter student ID to update: ");
+        // validate id
         int id;
         while(true){
+            System.out.print("Enter student ID to update: ");
             if(!sc.hasNextInt()){
-                System.out.println("Id must be a natural number!!");
+                System.out.println("Id must be a natural number!!!");
+                sc.nextLine();
+                continue;
             }
             id = sc.nextInt();
+            sc.nextLine();
             if(id < 0){
-                System.out.println("Id must be a natural number!!");
-            }else{
-                break;
+                System.out.println("Id must be positive!!!");
+                sc.nextLine();
+                continue;
             }
+            break;
             
         }
 
-        Student s = null;
-        for(Student student : studentDetails){
-            if(id == student.getId()){
-                s = student;
-                break;
-            }else {
-                s = null;
-            }
-        }
+        // Search if student with entered id exists.
+        Student s = sDao.searchById(id);
         if(null == s){
             System.out.println("Student with ID " + id + " not found.");
             return;
@@ -118,8 +98,13 @@ public class StudentService{
 
         System.out.print("Enter your new name: ");
         String name = sc.nextLine();
-        System.out.print("Enter your new email: ");
-        String email = sc.nextLine();
+
+        String email = null;
+        while(true){
+            System.out.print("Enter your new email: ");
+            email = sc.nextLine();
+            if(null == sDao.searchByEmail(email)) break;
+        }
 
         int age;
         while(true){
@@ -142,6 +127,7 @@ public class StudentService{
                 break;
             }
         }
+       
         System.out.print("Enter new course: ");
         String course = sc.nextLine();
 
@@ -149,55 +135,55 @@ public class StudentService{
         s.setEmail(email);
         s.setAge(age);
         s.setCourse(course);
+
+        sDao.update(s);
         
     }
 
     public void delete(Scanner sc){
-        System.out.println("Enter student ID to delete: ");
         int id;
         while(true){
+            System.out.print("Enter student ID to delete: ");
             if(!sc.hasNextInt()){
-                System.out.println("Id must be a natural number!!");
+                System.out.println("Id must be a natural number!!!");
+                sc.nextLine();
+                continue;
             }
             id = sc.nextInt();
             if(id < 0){
-                System.out.println("Id must be a natural number!!");
-            }else{
-                break;
+                System.out.println("Id must be positive!!!");
+                sc.nextLine();
+                continue;
             }
+            break;
             
         }
-        boolean found = false;
-        for(Student s : studentDetails){
-            if(id == s.getId()){
-                studentDetails.remove(s);
-                System.out.println("Student deleted successfully!");
-                found = true;
-                break;
-            }
+
+        if(sDao.searchById(id) == null){
+            System.out.println("Student with ID " + id + " not found.");
+            return;
         }
-        if(!found) System.out.println("Student not found.");
+
+        if(sDao.delete(id)){
+            System.out.println("Student deleted successfully!");
+            return;
+        }
     }
 
     public void search(Scanner sc){
         System.out.print("Enter email to search: ");
         String email = sc.nextLine();
 
-        boolean found = false;
+        Student s = sDao.searchByEmail(email);
 
-        for(Student s : studentDetails){
-            if(email.equals(s.getEmail())){
-                System.out.println("Student Details:");
-                System.out.println("-----------------------");
-                System.out.println("ID: " + s.getId());
-                System.out.println("Name: " + s.getName());
-                System.out.println("Email: " + s.getEmail());
-                System.out.println("Age: " + s.getAge());
-                System.out.println("Course: " + s.getCourse());
-                found = true;
-                break;
-            }
-        }
-        if(!found) System.out.println("No student found with this email.");
+        if(null == s) System.out.println("No student found with this email.");
+
+        System.out.println("Student Details:");
+        System.out.println("-----------------------");
+        System.out.println("ID: " + s.getId());
+        System.out.println("Name: " + s.getName());
+        System.out.println("Email: " + s.getEmail());
+        System.out.println("Age: " + s.getAge());
+        System.out.println("Course: " + s.getCourse());
     }
 }
